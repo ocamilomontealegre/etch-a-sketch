dbde933c6a6e9ee133aa1efbf036239b51f683ef
ocamilomontealegre <ocamilomontealegre@gmail.com>
2023-01-11 08:10:56 -0500

    Add getInputRangeValue to js/scripts.js
    This function adds an event listener to the inputRangeValue
    variable, so when is clicked, the values 1 and 2 change

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,30 @@ const getYear = () => {
 
 getYear();
 
+// Get Input Range Value
+// Save the grid-range element into a variable
+const inputRangeValue = document.querySelector("#grid-range");
+// Add an event listener to the inputRangeValue, so when
+// is clicked, the values 1 and 2 change
+const getInputRangeValue = () => {
+  inputRangeValue.addEventListener("click", () => {
+    const rangeValue1 = document.querySelector("#value-1");
+    const rangeValue2 = document.querySelector("#value-2");
+    rangeValue1.innerText = inputRangeValue.value;
+    rangeValue2.innerText = inputRangeValue.value;
+  })
+}
+
+getInputRangeValue();
+
+// Grid components
+
+// Save the grid main container into a variable
+const gridContainer = document.querySelector("#sketch-container");
+
+// Define the variable for creating a new div
+const gridComponent = document.createElement("div");
+
+
+
 
